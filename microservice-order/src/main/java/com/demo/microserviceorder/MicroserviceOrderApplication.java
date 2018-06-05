c9604ec9fdb4a25ff3ab68f44320aabfd130a6db
dell <13028592007@163.com>
2018-06-05 09:37:00 +0800

    增加zuul网关路由功能

--- microservice-order/src/main/java/com/demo/microserviceorder/MicroserviceOrderApplication.java
+++ microservice-order/src/main/java/com/demo/microserviceorder/MicroserviceOrderApplication.java
@@ -3,12 +3,15 @@ package com.demo.microserviceorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableFeignClients  //开启foeign
-//@EnableHystrix       //开启断路器
-//@EnableHystrixDashboard  //开启HystrixDashboard监控器
+@EnableFeignClients  //开启foeign
+@EnableHystrix       //开启断路器
+@EnableHystrixDashboard  //开启HystrixDashboard监控器
 public class MicroserviceOrderApplication {
 
 	public static void main(String[] args) {
--- microservice-user/src/main/java/com/demo/microserviceuser/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/com/demo/microserviceuser/MicroserviceUserApplication.java
@@ -3,6 +3,10 @@ package com.demo.microserviceuser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 
 /**
  * 使用注解@EnableEurekaClient开启注册功能，激活Eureka的DiscoveryClient实现
@@ -11,9 +15,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
-//@EnableFeignClients  //开启foeign
-//@EnableHystrix       //开启断路器
-//@EnableHystrixDashboard  //开启HystrixDashboard监控器
+@EnableFeignClients  //开启foeign
+@EnableHystrix       //开启断路器
+@EnableHystrixDashboard  //开启HystrixDashboard监控器
 public class MicroserviceUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceUserApplication.class, args);
